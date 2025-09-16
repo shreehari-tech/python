7f5819b06ce4df9c34e8e7ef37758844b92ba522
shreehari-tech <uma27logu@gmail.com>
2025-09-16 15:26:48 +0530

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,9 @@
+d=[]
+n=int(input("enter the no. of entries:"))
+for i in range(1,n):
+    key=input("enter the key")
+    value=input("enter the value")
+    d[key]=int(input(key))
+    d[values]=values
+print(d)
+    
